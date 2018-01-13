f4f266d960dbdf029821b5e87e703822898cdf5f
Vacity <141250206@smail.nju.edu.com>
2018-01-13 21:01:01 +0800

    创建项目
    创建项目

--- target/web-ssm/WEB-INF/js/finish.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    $("#userName").text(localStorage.getItem("name"));
-    $("#courseName").text(localStorage.getItem("studentCourseName"));
-    $("#studentId").text(localStorage.getItem("schoolId"));
-    // console.log(localStorage.getItem("name"))
-})
--- target/web-ssm/WEB-INF/js/isLogin.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-    if(localStorage.getItem("isLogin")=="0"){
-        top.location="index.jsp";
-    }
-})
